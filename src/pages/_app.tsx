3760f1d8cd2c99d5d0fd7e9c09dbafc51628d116
Muxammadbobur <bobur0172@gmail.com>
2023-02-25 09:49:38 -0800

    enable analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,41 +1,44 @@
 // src/pages/_app.tsx
-import { ChakraProvider } from '@chakra-ui/react';
-import type { Session } from 'next-auth';
-import { SessionProvider } from 'next-auth/react';
-import type { AppType } from 'next/app';
-import Router from 'next/router';
-import NProgress from 'nprogress';
-import { Toaster } from 'react-hot-toast';
-import Layout from '../components/layouts/main';
-import theme from '../lib/theme';
-import '../styles/globals.css';
-import '../styles/transition.css';
-import { trpc } from '../utils/trpc';
+import { ChakraProvider } from "@chakra-ui/react";
+import type { Session } from "next-auth";
+import { SessionProvider } from "next-auth/react";
+import type { AppType } from "next/app";
+import Router from "next/router";
+import NProgress from "nprogress";
+import { Toaster } from "react-hot-toast";
+import Layout from "../components/layouts/main";
+import theme from "../lib/theme";
+import "../styles/globals.css";
+import "../styles/transition.css";
+import { trpc } from "../utils/trpc";
+import { Analytics } from "@vercel/analytics/react";
+
 const MyApp: AppType<{ session: Session | null }> = ({
-	Component,
-	pageProps: { session, ...pageProps },
-	router,
+  Component,
+  pageProps: { session, ...pageProps },
+  router,
 }) => {
-	NProgress.configure({ showSpinner: false });
+  NProgress.configure({ showSpinner: false });
 
-	Router.events.on('routeChangeStart', () => {
-		NProgress.start();
-	});
+  Router.events.on("routeChangeStart", () => {
+    NProgress.start();
+  });
 
-	Router.events.on('routeChangeComplete', () => {
-		NProgress.done();
-	});
+  Router.events.on("routeChangeComplete", () => {
+    NProgress.done();
+  });
 
-	return (
-		<ChakraProvider theme={theme}>
-			<SessionProvider session={session}>
-				<Layout router={router}>
-					<Toaster />
-					<Component {...pageProps} />
-				</Layout>
-			</SessionProvider>
-		</ChakraProvider>
-	);
+  return (
+    <ChakraProvider theme={theme}>
+      <SessionProvider session={session}>
+        <Analytics />
+        <Layout router={router}>
+          <Toaster />
+          <Component {...pageProps} />
+        </Layout>
+      </SessionProvider>
+    </ChakraProvider>
+  );
 };
 
 export default trpc.withTRPC(MyApp);
